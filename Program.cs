ca21fbe1312fb1f81ad3419e8395c8014411ec15
Aleona Kozhukar <151907396+AlenaKozhukar@users.noreply.github.com>
2024-01-21 18:20:23 +0300

    Add files
    Алгоритм, решающий поставленную задачу, C#

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача: Написать программу, которая из имеющегося массива строк формирует 
+//новый массив из строк, длина которых меньше, либо равна 3 символам. 
+//Первоначальный массив можно ввести с клавиатуры, либо задать на старте 
+//выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+//лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+string [] lines = new string [] {"qWert", "yui", "op", "aSdf5", "-2", ":-)"};
+
+string shortsymbolSet = "";
+
+string GetShortSymbolSetFromArrayLine()
+{
+string result = "";
+
+for (int i = 0; i<lines.Length; i++)
+{
+    string symbolSet = lines [i];
+
+    if (symbolSet.Length > 3)
+    continue;
+    result = result + lines [i] + " ";
+}
+return result;
+}
+
+shortsymbolSet = GetShortSymbolSetFromArrayLine();
+System.Console.WriteLine(shortsymbolSet);
